9bad3ecd8c2de6f56cfe51598c268ba576e3b585
rui-watanabe <wataru610806@gmail.com>
2020-04-12 20:43:28 +0900

    hash password with bcrypt

--- models/user.js
+++ models/user.js
@@ -1,3 +1,5 @@
+const bcrypt = require('bcrypt')
+const saltRounds = 10
 const mongoose = require('mongoose')
 
 const userSchema = mongoose.Schema({
@@ -27,6 +29,21 @@ const userSchema = mongoose.Schema({
   },
   tokenExp: {
     type: Number
+  }
+})
+
+userSchema.pre('save', function(next){
+  var user  = this
+  if(user.isModified('password')){
+    bcrypt.genSalt(saltRounds, function(err, salt){
+      if(err) return next(err)
+      bcrypt.hash(user.password, salt, function(err, hash){
+        if(err) return next(err)
+        user.password = hash
+      })
+    })
+  } else {
+    next()
   }
 })
 
