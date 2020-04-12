86e5a693cd892697f08097f89a7c3283e182e630
rui-watanabe <wataru610806@gmail.com>
2020-04-12 19:06:14 +0900

    register function using postman

--- index.js
+++ index.js
@@ -2,13 +2,25 @@ const express = require('express')
 const app = express()
 require('dotenv').config()
 const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
+
+const { User } = require('./models/user')
 
 mongoose.connect(process.env.MONGODB_CONNECT, {useNewUrlParser: true})
   .then(() => console.log('DB connected'))
   .catch(err => console.error(err))
 
-app.use('/', (req,res) => {
-  res.send('hello world')
-}) 
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+app.use(cookieParser())
 
-app.listen(8000)
+app.post('/api/users/register', (req,res) => {
+  const user = new User(req.body)
+  user.save((err, userData) => {
+    if(err) return res.json({success: false, err})
+    return res.status(200).json({success: true})
+  })
+})
+
+app.listen(5000)
